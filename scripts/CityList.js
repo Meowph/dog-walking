fd4bc7d3a5930eb804b3fb4a6e55ce94b52feb8e
Meowph <bpardue33@gmail.com>
2024-04-25 13:27:06 -0400

    save

---
+++ scripts/CityList.js
@@ -0,0 +1,18 @@
+import { getWalkers } from "./database.js"
+
+
+const walkers = getWalkers()
+
+
+export const CityList = () => {
+    let citiesHTML = "<ul>"
+
+    for (const currentWalker of walkers) {
+        citiesHTML += `<li>${currentWalker.city}</li>`
+    }
+
+    citiesHTML += "</ul>"
+
+    return citiesHTML
+}
+
